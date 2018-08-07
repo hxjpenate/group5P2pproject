6d15d4fe014ac1f7fc6bf2780b7673c77073025d
sftofu <sftofu@outlook.com>
2018-08-07 08:43:23 +0800

    借款入口 borrow.html 申请借款 borrow_apply.html 借款详细信息 borrow_info.html?borrowid=7 三个静态页面静态样式完成

--- borrow_apply.php
+++ borrow_apply.php
@@ -26,15 +26,130 @@
     <?php
     include_once("./header.html");
     ?>
+        <!--借款信息页面的主题内容 -->
         <section class="container">
             <div class="row">
-
+                <!-- 左边栏的列表 -->
                 <div class="col-sm-3 left-list">
-                        <?php
+                    <?php
                         include_once("./leftList.html");
                         ?>
                 </div>
-                <div class="col-sm-9 right-info"></div>
+                <!-- 右边的信息及表单 -->
+                <div class="col-sm-9 right-info">
+                    <div class="title">
+                        <h3>信用借款</h3>
+                        <p>授信额度：
+                            <span>2000</span>     可借金额：
+                            <span>2000</span>
+                        </p>
+                    </div>
+                    <h3>借款信息
+                        <span class="label label-primary">信用标</span>
+                    </h3>
+                    <!-- 表单 -->
+                    <form class="form-horizontal" action="#" method="POST">
+                        <!-- 相关费用悬浮框 -->
+                        <div id="correlative">
+                            <h4>相关费用</h4>
+                            <p>利息
+                                <span>--</span>
+                            </p>
+                            <p>奖金
+                                <span>--</span>
+                            </p>
+                            <p>管理费
+                                <span>--</span>
+                            </p>
+                        </div>
+                        <!-- 借款金额 -->
+                        <div class="form-group row">
+                            <label for="money" class="col-sm-3 control-label">借款金额</label>
+                            <div class="input-group col-sm-3">
+                                <input type="text" class="form-control" name="money" id="money">
+                                <div class="input-group-addon">元</div>
+                            </div>
+                        </div>
+                        <!-- 借款利息 -->
+                        <div class="form-group row">
+                            <label for="interest" class="col-sm-3 control-label">借款利息</label>
+                            <div class="input-group col-sm-3">
+                                <input type="text" class="form-control" name="interest" id="interest">
+                                <div class="input-group-addon">%</div>
+                            </div>
+                        </div>
+                        <!-- 借款期限 -->
+                        <div class="form-group row">
+                            <label for="deadline" class="col-sm-3 control-label">借款期限</label>
+                            <div class="input-group col-sm-3">
+                                <select class="form-control" name="deadline" id="deadline">
+                                    <option>1</option>
+                                    <option>3</option>
+                                    <option>6</option>
+                                    <option>9</option>
+                                    <option selected>12</option>
+                                    <option>24</option>
+                                </select>
+                                <div class="input-group-addon">月</div>
+                            </div>
+                        </div>
+                        <!-- 还款方式 -->
+                        <div class="form-group row">
+                            <label for="way" class="col-sm-3 control-label">还款方式</label>
+
+                            <label class="radio-inline">
+                                <input type="radio" name="way" id="way" value="按月分期"> 按月分期
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="way" id="way" value="按月到期"> 按月到期
+                            </label>
+
+                        </div>
+
+                        <!-- 最小投标 -->
+                        <div class="form-group row">
+                            <label for="min-money" class="col-sm-3 control-label">最小投标</label>
+                            <div class="input-group col-sm-3">
+                                <input type="text" class="form-control" name="min-money" id="min-money">
+                                <div class="input-group-addon">元</div>
+                            </div>
+                        </div>
+                        <!-- 投标奖金 -->
+                        <div class="form-group row">
+                            <label for="bonus" class="col-sm-3 control-label">投标奖金</label>
+                            <div class="input-group col-sm-3">
+                                <input type="text" class="form-control" name="bonus" id="bonus">
+                                <div class="input-group-addon">%</div>
+                            </div>
+                        </div>
+                        <!-- 招标天数 -->
+                        <div class="form-group row">
+                            <label for="days" class="col-sm-3 control-label">招标天数</label>
+                            <div class="input-group col-sm-3">
+                                <input type="text" class="form-control" name="days" id="days">
+                                <div class="input-group-addon">天</div>
+                            </div>
+                        </div>
+                        <!-- 借款标题 -->
+                        <div class="form-group row">
+                            <label for="title" class="col-sm-3 control-label">借款标题</label>
+                            <div class="input-group col-sm-6 col-xs-12">
+                                <input type="text" class="form-control" name="title" id="title">
+                            </div>
+                        </div>
+                        <!-- 借款描述 -->
+                        <div class="form-group row">
+                            <label for="describe" class="col-sm-3 control-label">借款描述</label>
+                            <div class="input-group col-sm-6 col-xs-12">
+                                <textarea name="describe" id="describe" rows="4"></textarea>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary col-sm-offset-3">提交申请</button>
+
+                    </form>
+
+                </div>
+
 
             </div>
         </section>
---
+++ borrow_info.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- 引入重置样式 -->
+    <link rel="stylesheet" href="dist/css/reset.css">
+    <!--引入bootstrap样式-->
+    <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.min.css" />
+    <!--引入个人中心页面样式-->
+    <link rel="stylesheet" type="text/css" href="dist/css/borrow_info.min.css" />
+    <!-- 引入字体图标库 -->
+    <link rel="stylesheet" href="lib/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--兼容低版本的浏览器-->
+    <!--[if lt IE 9]>
+        <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <!-- 页头 -->
+    <?php
+    include_once("./header.html");
+    ?>
+        <!--个人中心页面的主题内容 -->
+        <section class="container">
+            <!-- 个人信息 -->
+            <div class="row sin-info">
+                <div class="col-sm-3">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">借款人</div>
+                        <div class="panel-body">
+                            <img src="static/images/head_icon.jpg" alt="头像">
+                            <a href="#">
+                                <span>埃利斯</span>
+                            </a>
+                            <p>籍贯： 四川 - 成都</p>
+                            <p>专职客服：
+                                <a href="#">码妹妹</a>
+                            </p>
+                            <p>认证信息：
+                                <i class="fa fa-user-circle" aria-hidden="true"></i>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <h3>埃利斯借款
+                        <small>
+                            <span class="label label-primary">信</span>
+                        </small>
+                    </h3>
+                    <div class="row">
+                        <p class="col-sm-2">
+                            借款金额
+                        </p>
+                        <p class="col-sm-10">
+                            2,000.00
+                        </p>
+                    </div>
+                    <div class="row">
+                        <p class="col-sm-2">
+                            年化利率
+                        </p>
+                        <p class="col-sm-10">
+                            10.00%
+                        </p>
+                    </div>
+                    <div class="row">
+                        <p class="col-sm-2">
+                            借款期限
+                        </p>
+                        <p class="col-sm-10">
+                            3月
+                        </p>
+                    </div>
+                    <div class="row">
+                        <p class="col-sm-2">
+                            投标奖励
+                        </p>
+                        <p class="col-sm-10">
+                            0.00
+                        </p>
+                    </div>
+                    <div class="row">
+                        <p class="col-sm-2">
+                            还款方式
+                        </p>
+                        <p class="col-sm-10">
+                            按月分期还款
+                        </p>
+                    </div>
+                    <div class="row">
+                        <p class="col-sm-2">
+                            最小投标
+                        </p>
+                        <p class="col-sm-10">
+                            20.00
+                        </p>
+                    </div>
+                    <div class="row">
+                        <p class="col-sm-2">
+                            剩余时间
+                        </p>
+                        <p class="col-sm-10">
+
+                        </p>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="row">
+                        <p class="col-sm-6">投标总数</p>
+                        <p class="col-sm-6">2</p>
+                    </div>
+                    <div class="row">
+                        <p class="col-sm-6">还需金额</p>
+                        <p class="col-sm-6">0.00元</p>
+                    </div>
+                    <div class="row">
+                        <p class="col-sm-6">投标进度</p>
+                        <p class="col-sm-6"></p>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0"
+                            aria-valuemax="100" style="width: 40%">
+                            <span class="sr-only">40% Complete (success)</span>
+                        </div>
+                    </div>
+                    <h4>还款中</h4>
+                </div>
+            </div>
+            <!-- 借款人信息 -->
+            <div class="panel panel-default borrower-info">
+                <div class="panel-heading">借款人信息</div>
+                <div class="panel-body">
+                    <div class="row">
+                        <p class="col-sm-1">注册时间</p>
+                        <p class="col-sm-2">2015-03-30</p>
+                        <p class="col-sm-1">借款额度</p>
+                        <p class="col-sm-2">2000</p>
+                        <p class="col-sm-1">性别</p>
+                        <p class="col-sm-2">男</p>
+                        <p class="col-sm-1">年龄</p>
+                        <p class="col-sm-2">25</p>
+                        <p class="col-sm-1">文化程度</p>
+                        <p class="col-sm-2">硕士研究生</p>
+                        <p class="col-sm-1">每月收入</p>
+                        <p class="col-sm-2">3000-4999</p>
+                    </div>
+                </div>
+            </div>
+            <!-- 审核认证信息 -->
+            <div class="panel panel-default approve-info">
+                <div class="panel-heading">审核认证信息</div>
+                <div class="panel-body">
+                    <div class="row">
+                        <p class="col-sm-2">认证时态度</p>
+                        <p class="col-sm-1">优</p>
+                        <p class="col-sm-2">信贷知识掌握情况</p>
+                        <p class="col-sm-1">优</p>
+                        <p class="col-sm-2">借款感言</p>
+                        <p class="col-sm-4">给我2000度过难关</p>
+                        <p class="col-sm-2">审核员认证意见</p>
+                        <p class="col-sm-10">啊啊啊啊啊</p>
+                    </div>
+                </div>
+            </div>
+            <!-- 材料信息 -->
+            <div class="panel panel-default">
+                <div class="panel-heading">材料信息</div>
+                <div class="panel-body">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>材料类型</th>
+                                <th>材料数量</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>本人身份证</td>
+                                <td>2</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- 还款情况 -->
+            <div class="panel panel-default">
+                <div class="panel-heading">还款情况</div>
+                <div class="panel-body">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>还款状态</th>
+                                <th>最近一周</th>
+                                <th class="hide420">最近1月</th>
+                                <th class="hide420">最近6月</th>
+                                <th class="hide420">6个月前</th>
+                                <th>总计[?]</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>提前还款</td>
+                                <td>0</td>
+                                <td class="hide420">0</td>
+                                <td class="hide420">0</td>
+                                <td class="hide420">0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>准时还款</td>
+                                <td>0</td>
+                                <td class="hide420">0</td>
+                                <td class="hide420">0</td>
+                                <td class="hide420">0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>逾期已还</td>
+                                <td>0</td>
+                                <td class="hide420">0</td>
+                                <td class="hide420">0</td>
+                                <td class="hide420">0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>逾期未还</td>
+                                <td>0</td>
+                                <td class="hide420">0</td>
+                                <td class="hide420">0</td>
+                                <td class="hide420">0</td>
+                                <td>0</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- 投标记录 -->
+            <div class="panel panel-default">
+                <div class="panel-heading">投标记录</div>
+                <div class="panel-body">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>投标人</th>
+                                <th>年利率</th>
+                                <th>有效金额(¥)</th>
+                                <th class="hide420">投标时间</th>
+                                <th class="hide420">类型</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>si*****</td>
+                                <td>10%</td>
+                                <td>1,000.00</td>
+                                <td class="hide420">2016-05-11 06:42:11</td>
+                                <td class="hide420">手动投标</td>
+                            </tr>
+                            <tr>
+                                <td>xia*****</td>
+                                <td>10%</td>
+                                <td>1,000.00</td>
+                                <td class="hide420">2016-05-12 13:44:01</td>
+                                <td class="hide420">手动投标</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </section>
+        <!-- 页脚 -->
+        <?php
+    include_once("./footer.html");
+    ?>
+
+            <!--引入jquery-->
+            <script src="lib/jquery/jquery.js" type="text/javascript" charset="utf-8"></script>
+            <!--引入bootstrap的js库-->
+            <script src="lib/bootstrap/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
+            <!-- 引入自定义工具样式 -->
+            <script src="dist/js/tools.min.js"></script>
+</body>
+
+</html>
