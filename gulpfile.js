f4dbe7a7518312868b910e4bdac1426d0285de62
sftofu <sftofu@outlook.com>
2018-08-07 19:05:36 +0800

    停止js合并

--- gulpfile.js
+++ gulpfile.js
@@ -86,7 +86,7 @@ gulp.task('uglifyImg', function () {
 // 观察者
 gulp.task("default", function () {
     gulp.watch("src/js/*.js", ["uglifyJs"]);
-    gulp.watch("src/js/*.js", ["scripts"]);
+    // gulp.watch("src/js/*.js", ["scripts"]);
     gulp.watch("src/less/*.less", ["LessTocss"]);
     gulp.watch("src/less/*.less", ["uglifyLess"]);
     gulp.watch("src/sass/*.scss", ["sass"]);
